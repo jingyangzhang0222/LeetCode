ec02f2e0f2c8701d9fa5bf1a63406078c7be8dce
Jingyang Zhang <jingyangzhang@172-16-40-179.DYNAPOOL.NYU.EDU>
2018-04-24 21:48:33 -0400

    0424

--- NestedListWeightSum.java
+++ NestedListWeightSum.java
@@ -3,7 +3,7 @@ package leetcode;
 public class NestedListWeightSum {
     public static void main (String[] args) {
         NestedListWeightSum sol = new NestedListWeightSum();
-        String test = "[1]";
+        String test = "{{1,3},5,7,{9,{1, 2},6,4}}";
         int res = sol.depthSum(test);
         System.out.print(res);
     }
@@ -16,16 +16,21 @@ public class NestedListWeightSum {
         int depth = 0;
         int num = 0;
         for (int i = 0; i < nestlists.length(); i++) {
-            if (nestlists.charAt(i) == '[') {
-                depth++;
+            if (nestlists.charAt(i) == '{') {
+                res += depth++ * num;
+                num = 0;
             } else if (nestlists.charAt(i) == ',') {
                 res += depth * num;
                 num = 0;
-            } else if (nestlists.charAt(i) == ']') {
+            } else if (nestlists.charAt(i) == '}') {
                 res += depth-- * num;
                 num = 0;
-            } else {
-                num = 10 * num + (int)(nestlists.charAt(i) - '0');
+            } else if (nestlists.charAt(i) - '0' >= 0 && nestlists.charAt(i) - '0' <= 9) {
+                if (num < 0 || i - 1 >= 0 && nestlists.charAt(i - 1) == '-') {
+                    num = 10 * num - (int)(nestlists.charAt(i) - '0');
+                } else {
+                    num = 10 * num + (int)(nestlists.charAt(i) - '0');
+                }
             }
         }
         return res;
