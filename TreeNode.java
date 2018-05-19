95ad3946a4d48d46d1c8bfcd3b435ffd999c685c
Jingyang Zhang <jingyangzhang@Jingyangs-MBP.fios-router.home>
2018-05-19 19:46:47 -0400

    0519

---
+++ TreeNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class TreeNode {
+
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int key) {
+        this.key = key;
+    }
+}
