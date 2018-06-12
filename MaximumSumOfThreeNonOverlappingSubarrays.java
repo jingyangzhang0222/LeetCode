8ba9043691f500400af8d39a1176b2d1cff9d2a9
Jingyang Zhang <jingyangzhang@Jingyangs-MBP.fios-router.home>
2018-06-12 00:02:54 -0400

    0611

--- MaximumSumOfThreeNonOverlappingSubarrays.java
+++ MaximumSumOfThreeNonOverlappingSubarrays.java
@@ -1,72 +1,83 @@
+/*
+* In a given array nums of positive integers, find three non-overlapping subarrays with maximum sum.
+
+Each subarray will be of size k, and we want to maximize the sum of all 3*k entries.
+
+Return the result as a list of indices representing the starting position of each interval (0-indexed). If there are multiple answers, return the lexicographically smallest one.
+
+Example:
+
+Input: [1,2,1,2,6,7,5,1], 2
+Output: [0, 3, 5]
+Explanation: Subarrays [1, 2], [2, 6], [7, 5] correspond to the starting indices [0, 3, 5].
+We could have also taken [2, 1], but an answer of [1, 3, 5] would be lexicographically larger.
+Note:
+
+nums.length will be between 1 and 20000.
+nums[i] will be between 1 and 65535.
+k will be between 1 and floor(nums.length / 3).
+
+    O(n)
+    O(n)
+* */
 package leetcode;
 
 public class MaximumSumOfThreeNonOverlappingSubarrays {
     public static void main(String[] args) {
         MaximumSumOfThreeNonOverlappingSubarrays test = new MaximumSumOfThreeNonOverlappingSubarrays();
-        int[] res = test.maxSumOfThreeSubarrays(new int[1], 4);
+        int[] input = {1, 2, 1, 2, 6, 7, 5, 1};
+        int[] res = test.maxSumOfThreeSubarrays(input, 2);
         for (int num : res) {
             System.out.println(num);
         }
     }
+
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-        int[] prefix = new int[nums.length];
+        int[] tmp = new int[nums.length - (k - 1)];
+
+        int[] one = new int[tmp.length];
+        int[][] two = new int[tmp.length][];
+        int[][] three = new int[tmp.length][];
+
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                prefix[i] = nums[i];
-            } else {
-                prefix[i] = prefix[i - 1] + nums[i];
+            sum += nums[i];
+            if (i >= k - 1) {
+                tmp[i - k + 1] = sum;
+                sum -= nums[i - k + 1];
             }
         }
-        /*
-        int[] newNums = new int[nums.length - k + 1];
-        for (int i = 0; i < newNums.length; i++) {
-            newNums[i] = prefix[i + k - 1] - prefix[i] + nums[i];
-        }
-        */
-        int[] newNums = new int[] {5,3,1,4,8,1,2,3,7,6,9,5};
-        int[] one = new int[k];
-        int[] two = new int[k];
-        int[] three = new int[k];
 
-        int firstIndex = 0;
-        int[] secondIndexes = new int[2];
-        int[] thirdIndexes = new int[3];
-
-        int globalMax = Integer.MIN_VALUE;
-
-        for (int i = 0; i < newNums.length; i++) {
-            int index = i % k;
-            int prevIndex = index == 0 ? k - 1 : index - 1;
-
-            if (i >= 2 * k) {
-                if (two[index] + newNums[i] > three[prevIndex]) {
-                    three[index] = two[index] + newNums[i];
-                    thirdIndexes[2] = i;
-                    thirdIndexes[1] = secondIndexes[1];
-                    thirdIndexes[0] = secondIndexes[0];
-                    globalMax = three[index];
-                } else {
-                    three[index] = three[prevIndex];
-                }
+        for (int i = 0; i < tmp.length; i++) {
+            int prevOne = i == 0 ? 0 : tmp[one[i - 1]];
+            if (tmp[i] > prevOne) {
+                one[i] = i;
+            } else {
+                one[i] = one[i - 1];
             }
 
             if (i >= k) {
-                if (one[index] + newNums[i] > two[prevIndex]) {
-                    two[index] = one[index] + newNums[i];
-                    secondIndexes[1] = i;
-                    secondIndexes[0] = firstIndex;
+                int prevTwo = i == k ? 0 : tmp[two[i - 1][0]] + tmp[two[i - 1][1]];
+                int candidate = tmp[i] + tmp[one[i - k]];
+                if (candidate > prevTwo) {
+                    two[i] = new int[]{one[i - k], i};
                 } else {
-                    two[index] = two[prevIndex];
+                    two[i] = two[i - 1];
                 }
             }
 
-            if (newNums[i] > one[prevIndex]) {
-                one[index] = newNums[i];
-                firstIndex = i;
-            } else {
-                one[index] = one[prevIndex];
+            if (i >= 2 * k) {
+                int prevThree = i == 2 * k ? 0 : tmp[three[i - 1][0]] +
+                        tmp[three[i - 1][1]] + tmp[three[i - 1][2]];
+                int candidate = tmp[i] + tmp[two[i - k][0]] + tmp[two[i - k][1]];
+                if (candidate > prevThree) {
+                    three[i] = new int[]{two[i - k][0], two[i - k][1], i};
+                } else {
+                    three[i] = three[i - 1];
+                }
             }
         }
-        return thirdIndexes;
+
+        return three[three.length - 1];
     }
 }
--- TextJustification.java
+++ TextJustification.java
@@ -1,74 +1,64 @@
+/*
+Given an array of words and a width maxWidth, format the text such that each line has
+exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can
+in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of
+spaces on a line do not divide evenly between words, the empty slots on the left will be
+assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left justified and no extra space is inserted between words.
+
+Note:
+
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+Example 1:
+
+Input:
+words = ["This", "is", "an", "example", "of", "text", "justification."]
+maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+Example 2:
+
+Input:
+words = ["What","must","be","acknowledgment","shall","be"]
+maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be",
+             because the last line must be left-justified instead of fully-justified.
+             Note that the second line is also left-justified becase it contains only one word.
+Example 3:
+
+Input:
+words = ["Science","is","what","we","understand","well","enough","to","explain",
+         "to","a","computer.","Art","is","everything","else","we","do"]
+maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+*/
 package leetcode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-
 public class TextJustification {
-    /* todo
-    public ArrayList<String> fullJustify(String[] words, int L) {
-        ArrayList<String> sol = new ArrayList<>();
-        Deque<String> tmp = new ArrayDeque<>();
-        int wordsLength = 0;
-        int actualLength = 0;
-        for (String s : words) {
-            int curLength = s.length();
-            if (curLength + actualLength == L) {
-                dealWithSingle(tmp, sol);
-                wordsLength = 0;
-                actualLength = 0;
-            } else if (curLength + actualLength < L) {
-                tmp.offerLast(s);
-                wordsLength += curLength;
-                actualLength += curLength + 1;
-            } else { // curLength + actualLength > L
-                dealWithMul(tmp, sol, wordsLength, L);
-                wordsLength = 0;
-                actualLength = 0;
-            }
-        }
-        dealWithLast();
-        return sol;
-    }
 
-    private void dealWithSingle(Deque<String> tmp, ArrayList<String> sol) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(tmp.pollFirst());
-        while (!tmp.isEmpty()) {
-            sb.append(tmp.pollFirst());
-            sb.append(' ');
-        }
-        sol.add(sb.toString());
-    }
-
-    private void dealWithMul(Deque<String> tmp, ArrayList<String> sol, int wordsLength, int L) {
-        StringBuilder sb = new StringBuilder();
-        int spaces = L - wordsLength;
-        int each = tmp.size() == 1 ? spaces : spaces / (tmp.size() - 1);
-        int left = spaces - each * (tmp.size() - 1);
-        sb.append(tmp.pollFirst());
-        if (tmp.isEmpty()) {
-            for (int i = 0; i < each; i++) {
-                sb.append(' ');
-            }
-        }
-        while (!tmp.isEmpty()) {
-            for (int i = 0; i < each; i++) {
-                sb.append(' ');
-            }
-            if (left != 0) {
-                sb.append(' ');
-                left--;
-            }
-            sb.append(tmp.pollFirst());
-        }
-        sol.add(sb.toString());
-    }
-
-    private void dealWithLast(Deque<String> tmp, ArrayList<String> sol, int wordsLength, int L) {
-        StringBuilder sb = new StringBuilder();
-        int spaces = L - wordsLength;
-
-    }
-    */
 }
